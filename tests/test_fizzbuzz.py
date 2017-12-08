40a850358f4c526b2ccb0c38e50efe4920483b54
James Prior <jep200404@columbus.rr.com>
2017-12-07 19:07:07 -0500

    tests/test_fizzbuzz.py tests/__init__.py fizzbuzz.py: Test that 1 -> 1.

---
+++ tests/test_fizzbuzz.py
@@ -0,0 +1,26 @@
+import pytest
+
+from fizzbuzz import fizzbuzz
+
+number_to_expected_string = {
+    1: '1',
+}
+@pytest.mark.parametrize('number, expected_string', number_to_expected_string.items())
+def test_known_number_returns_expected(number, expected_string):
+    assert expected_string == fizzbuzz(number) 
+
+# bad_values_to_expected_error = {
+#     -1: ValueError,
+#     0: ValueError,
+#     1: ValueError,
+#     'hello': ValueError,
+# }
+# @pytest.mark.parametrize('bad_value, expected_error', bad_values_to_expected_error.items())
+# def test_known_value_returns_expected_error(bad_value, expected_error):
+#     # Rendundant tests show alternate syntax.
+# 
+#     with pytest.raises(expected_error):
+#         largest_factor(bad_value)
+# 
+#     # older syntax works with 2.4
+#     pytest.raises(expected_error, "largest_factor(bad_value)")
