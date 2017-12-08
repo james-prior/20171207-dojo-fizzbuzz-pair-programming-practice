46a1f9d67c16dbc2f638d7e3d14962d6861d7fc4
James Prior <jep200404@columbus.rr.com>
2017-12-07 19:19:31 -0500

    tests/test_fizzbuzz.py fizzbuzz.py: Test that multiples of five yield 'buzz'.

--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -8,6 +8,7 @@ number_to_expected_string = {
     3: 'fizz',
     6: 'fizz',
     5: 'buzz',
+    10: 'buzz',
 }
 @pytest.mark.parametrize('number, expected_string', number_to_expected_string.items())
 def test_known_number_returns_expected(number, expected_string):
